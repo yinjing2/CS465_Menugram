f50874d0d17ba9012583f2586b7c24a9c5ba42b7
HanWang <hanw7@illinois.edu>
2019-11-19 20:11:12 -0600

    add db

--- app/src/main/java/com/example/cs465_menugram/MainActivity.java
+++ app/src/main/java/com/example/cs465_menugram/MainActivity.java
@@ -3,32 +3,79 @@ package com.example.cs465_menugram;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    public static boolean true_clicked = false;
-    public static boolean false_clicked = false;
-    private Button reviewButton;
+    private ImageButton pic;
+    private TextView title;
+    private TextView restName;
+
+    // Read from the database
+//        myRef.addValueEventListener(new ValueEventListener() {
+//        @Override
+//        public void onDataChange(DataSnapshot dataSnapshot) {
+//            // This method is called once with the initial value and again
+//            // whenever data at this location is updated.
+//            String value = dataSnapshot.getValue(String.class);
+//            Log.d("MAIN_ACTIVITY", "Value is: " + value);
+//        }
+//
+//        @Override
+//        public void onCancelled(DatabaseError error) {
+//            // Failed to read value
+//            Log.w(TAG, "Failed to read value.", error.toException());
+//        }
+//    });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        reviewButton = (Button) findViewById(R.id.button);
+        pic = (ImageButton) findViewById(R.id.dishOne);
+        title = (TextView) findViewById(R.id.name);
+        restName = (TextView) findViewById(R.id.restName);
 
-        reviewButton.setOnClickListener(this);
+
+//        // Write a message to the database
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("message");
+//
+//        // Read from the database
+//        myRef.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                // This method is called once with the initial value and again
+//                // whenever data at this location is updated.
+//                String value = dataSnapshot.getValue(String.class);
+//                Log.d("MAIN_ACTIVITY", "Value is: " + value);
+//            }
+//
+//            @Override
+//            public void onCancelled(DatabaseError error) {
+//                // Failed to read value
+//                Log.w("MAIN_ACTIVITY", "Failed to read value.", error.toException());
+//            }
+//        });
+
+
+        pic.setOnClickListener(this);
+        title.setOnClickListener(this);
+        restName.setOnClickListener(this);
     }
 
     public void onClick(View v) {
-        if (v.getId() == R.id.button) {
-            true_clicked = true;
-            Intent intent = new Intent(this, ReviewActivity.class);
-            intent.putExtra("is_True", "true");
+        if (v.getId() == R.id.name || v.getId() == R.id.restName || v.getId() == R.id.dishOne) {
+            TextView tView = findViewById(R.id.restName);
+            Intent intent = new Intent(this, MenuActivity.class);
+            intent.putExtra("rest_name", tView.getText());
             startActivity(intent);
         }
     }
+    // Read from the database
+
 }
--- app/src/main/java/com/example/cs465_menugram/MenuActivity.java
+++ app/src/main/java/com/example/cs465_menugram/MenuActivity.java
@@ -1,4 +1,128 @@
 package com.example.cs465_menugram;
 
-public class MenuActivity {
+import android.app.AppComponentFactory;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import net.gotev.uploadservice.MultipartUploadRequest;
+import net.gotev.uploadservice.UploadNotificationConfig;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MenuActivity extends AppCompatActivity {
+
+    private String restaurantName;
+    private TextView dishName1;
+    private TextView dishName2;
+    private TextView dishName3;
+    private TextView dishName4;
+    private TextView dishDescription1;
+    private TextView dishDescription2;
+    private TextView dishDescription3;
+    private TextView dishDescription4;
+    private TextView dishPrice1;
+    private TextView dishPrice2;
+    private TextView dishPrice3;
+    private TextView dishPrice4;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+
+        Bundle extras = getIntent().getExtras();
+
+        if(extras != null) {
+            restaurantName = extras.getString("rest_name");
+            Log.d("MENU_ACTIVITY", "RestaurantName is: " + restaurantName);
+        }
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Restaurants/"+restaurantName+"/Menu/");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Map<String, Object> map = (Map<String, Object>)dataSnapshot.getValue();
+                Log.d("MENU_ACTIVITY", "Map is: " + map.toString());
+                Log.d("MENU_ACTIVITY", "Map keys are: " + map.keySet().toString());
+
+                Set<String> restaurant_name_set = map.keySet();
+
+
+                Log.d("MENU_ACTIVITY", (String)map.get((String)restaurant_name_set.toArray()[0]));
+
+
+                //Set names of textViews
+                dishName1 = (TextView)findViewById(R.id.dishOne);
+                dishName1.setText((String)restaurant_name_set.toArray()[0]);
+
+                dishName2 = (TextView)findViewById(R.id.dishOne);
+                dishName2.setText((String)restaurant_name_set.toArray()[0]);
+
+                dishName3 = (TextView)findViewById(R.id.dishThree);
+                dishName3.setText((String)restaurant_name_set.toArray()[2]);
+
+                dishName4 = (TextView)findViewById(R.id.dishFour);
+                dishName4.setText((String)restaurant_name_set.toArray()[3]);
+
+
+//                (TextView)findViewById(R.id.priceOne);
+//                (TextView)findViewById(R.id.priceTwo);
+//                (TextView)findViewById(R.id.priceThree);
+//                (TextView)findViewById(R.id.priceFour);
+//                (TextView)findViewById(R.id.descriptionOne);
+//                (TextView)findViewById(R.id.descriptionTwo);
+//                (TextView)findViewById(R.id.descriptionThree);
+//                (TextView)findViewById(R.id.descriptionFour);
+//
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("MENU_ACTIVITY", "Failed to read value.", error.toException());
+            }
+        });
+
+    }
+
+
 }
